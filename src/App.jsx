c6f094796f72988d2cf5f0a941b8cbd50d280e36
GaleArt <GaleguzovAA@gmail.com>
2023-01-10 20:29:45 +0300

    Стилизуем "Потребители"

--- src/App.jsx
+++ src/App.jsx
@@ -40,19 +40,25 @@ function App() {
 					<Route
 						index
 						element={
-							<div className='container'>
+							<div className='md:container md:mx-auto flex justify-center'>
 								<div>
 									<label
 										for='UserEmail'
-										class='relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'
+										className='relative mt-3 block overflow-hidden rounded-md border 
+										border-gray-200 px-3 pt-3 shadow-sm 
+										focus-within:border-blue-600 focus-within:ring-1 
+										focus-within:ring-blue-600'
 									>
 										<input
 											placeholder='Наименование потребителя'
-											class='peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm'
+											className='peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm'
 											onChange={(e) => setSearchTerm(e.target.value)}
 										/>
 
-										<span class='absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs'>
+										<span
+											class='absolute left-3 top-2 -translate-y-1/2 text-xs 
+										text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs'
+										>
 											Наименование потребителя
 										</span>
 									</label>
