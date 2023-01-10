4534a374c459e755477c1e5b8e886a3caa97eb08
GaleArt <GaleguzovAA@gmail.com>
2023-01-10 18:51:45 +0300

    в проект добавлен tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import CalcRiz from './Components/Calcs/CalcRiz';
 import CalcUt from './Components/Calcs/CalcUt';
 import Todo from './Components/TodoList/Todo/Todo';
 
-
 import CONNECT_DATA from './db/base.json';
 
 const filterConnects = (searchText, listOfConnects) => {
@@ -36,22 +35,38 @@ function App() {
 
 	return (
 		<>
-		
-		<Routes>
-			<Route path='/' element={<NavBar />}>
-			<Route index element={
-				<div className='container'>
-					<div>
-						<input onChange={(e) => setSearchTerm(e.target.value)} />
-						<ConnectList connectList={connectList} />
-					</div>
-				</div>
-			} />
-			<Route path='calcriz' element={<CalcRiz />} />
-			<Route path='calcut' element={<CalcUt />} />
-			<Route path='todo' element={<Todo />} />
-			</Route>
-		</Routes>
+			<Routes>
+				<Route path='/' element={<NavBar />}>
+					<Route
+						index
+						element={
+							<div className='container'>
+								<div>
+									<label
+										for='UserEmail'
+										class='relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600'
+									>
+										<input
+											placeholder='Наименование потребителя'
+											class='peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm'
+											onChange={(e) => setSearchTerm(e.target.value)}
+										/>
+
+										<span class='absolute left-3 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs'>
+											Наименование потребителя
+										</span>
+									</label>
+
+									<ConnectList connectList={connectList} />
+								</div>
+							</div>
+						}
+					/>
+					<Route path='calcriz' element={<CalcRiz />} />
+					<Route path='calcut' element={<CalcUt />} />
+					<Route path='todo' element={<Todo />} />
+				</Route>
+			</Routes>
 		</>
 	);
 }
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <>
-    <header>
-	<Link to="/">Потребители</Link>
-	<Link to="/calcriz">Калькулятор Rиз</Link>
-  <Link to="/calcut">Суточная утечка</Link>
-  <Link to="/todo">Напоминалка</Link>
-	</header>
+	return (
+		<>
+			<header>
+				<Link to='/'>Потребители</Link>
+				<Link to='/calcriz'>Калькулятор Rиз</Link>
+				<Link to='/calcut'>Суточная утечка</Link>
+				<Link to='/todo'>Напоминалка</Link>
+			</header>
 
-    <Outlet />
+			<Outlet />
 
-    <footer>2023</footer>
-    </>
-  )
-}
+			<footer>2023</footer>
+		</>
+	);
+};
 
-export { NavBar }
+export { NavBar };
