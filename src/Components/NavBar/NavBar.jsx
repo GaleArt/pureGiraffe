fb8c701183c6a7f0155484d982d14e22a86a2d3b
GaleArt <GaleguzovAA@gmail.com>
2023-01-11 23:32:04 +0300

    изучаем tailwind css

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,12 +4,34 @@ import { Link, Outlet } from 'react-router-dom';
 const NavBar = () => {
 	return (
 		<>
-			<header>
-				<Link to='/'>Потребители</Link>
-				<Link to='/calcriz'>Калькулятор Rиз</Link>
-				<Link to='/calcut'>Суточная утечка</Link>
-				<Link to='/todo'>Напоминалка</Link>
-			</header>
+			<nav className='h-10 bg-orange-200'>
+				<ul className='flex items-center justify-around h-full p-1.5'>
+					<Link
+						className='bg-gradient-to-r from-cyan-500 to-blue-500 p-2 rounded-2xl'
+						to='/'
+					>
+						Потребители
+					</Link>
+					<Link
+						className='bg-gradient-to-r from-cyan-500 to-blue-500'
+						to='/calcriz'
+					>
+						Калькулятор Rиз
+					</Link>
+					<Link
+						className='bg-gradient-to-r from-cyan-500 to-blue-500'
+						to='/calcut'
+					>
+						Суточная утечка
+					</Link>
+					<Link
+						className='bg-gradient-to-r from-cyan-500 to-blue-500'
+						to='/todo'
+					>
+						Напоминалка
+					</Link>
+				</ul>
+			</nav>
 
 			<Outlet />
 
